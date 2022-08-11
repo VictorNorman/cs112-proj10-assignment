a0c7f76a6950204b8418ed478ac938b4c622f71b
VictorNorman <vtn2@calvin.edu>
2022-08-11 10:08:35 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include "PalindromeDetector.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	// string inFileName, outFileName;
+
+	// cout << "Welcome to the palindrome detector." << endl << endl;
+
+	// cout << "Please enter the name of the input file: ";
+	// cin >> inFileName;
+	// cout << "Please enter the name of the output file: ";
+	// cin >> outFileName;
+	// PalindromeDetector pd(inFileName, outFileName);
+	// pd.detectPalindromes();
+	// cout << "done\n";
+}
